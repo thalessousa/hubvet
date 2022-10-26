e479cd3e1f5b82d7ca07cd999a209c5a785e8632
Thales Sousa <tsousa@easyjur.com>
2022-10-26 17:38:05 -0300

    feat : added api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::group(
+    [
+        'middleware' => ['auth:sanctum'],
+    ],
+    function () {
+        Route::apiResource('products', 'ProductController');
+        Route::apiResource('sectors', 'SectorController');
+        Route::apiResource('stores', 'StoreController');
+        Route::apiResource('users', 'UserController')->except(['update']);
+        Route::prefix('users')->group(function () {
+            Route::controller(UserController::class)->group(function () {
+                Route::get('', 'login');
+                Route::get('', 'login');
+                Route::delete('{user}', 'destroy');
+            });
+        });
+    }
+);
